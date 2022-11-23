dbf0138ff897729c9d796ee78f3f7de3b378a377
Juan <juaniak109@outlook.es>
2022-11-22 19:44:18 -0600

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,23 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register</title>
+    <title>Registro</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
-    <form action="{{ route('register') }}" method="post">
-        @csrf
-        <label for="name">Name</label>
-        <input type="text" name="name">
-        
-        <input type="email" name="email">
-        <input type="password" name="password">
-        <input type="password" name="password_confirmation">
-        <input type="text" name="phone">
-        <input type="text" name="address">
-        <button>Submit</button>
-    </form>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+
+            </div>
+            <div class="col-md-6">
+                <br><br><br>
+                <div class="card">
+                    <div class="card-header">
+                        Registro
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('register') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Nombre</label>
+                                <input type="text" name="name" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">Correo</label>
+                                <input type="email" name="email" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">Contraseña</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">Confirmar Contraseñas</label>
+                                <input type="password" name="password_confirmation" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="phone">Telefono</label>
+                                <input type="phone" name="phone" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="address">Dirección</label>
+                                <input type="address" name="address" class="form-control">
+                            </div>
+
+                            <button class="btn btn-primary">Registrarse</button>
+
+                        </form>
+                    </div>
+                </div>
+                <br>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
