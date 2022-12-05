64146b7413da9561658b6f5548abc92197d00e33
Juan <juaniak109@outlook.es>
2022-12-04 21:58:29 -0600

    admin

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="{{ asset('storage/static/admin-icon.png') }}" width="13%">
-    <h1 class="display-5 fw-bold">Bienvenido {{auth()->user()->name}}</h1>
+    <img class="d-block mx-auto mb-4" src="https://cdn-icons-png.flaticon.com/512/2345/2345338.png" width="13%">
+    <h1 class="display-5 fw-bold">Bienvenido Administrador</h1>
     <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Navega por las opciones para editar, agregar o eliminar las secciones que necesites del sitio</p>
+      <p class="lead mb-4">Navege por las opciones para editar, agregar o eliminar las secciones que necesites del sitio</p>
     </div>
   </div>
 @endsection
